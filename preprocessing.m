d09a9728c200214e30afe21e22bb19e1cd0c9df0
Broekhoven, Jens van <j.g.a.m.v.broekhoven@student.tue.nl>
2026-05-15 14:51:50 +0200

    added outside possibility of tank

--- preprocessing.m
+++ preprocessing.m
@@ -22,6 +22,7 @@ Demand = loadDemandData(demandFile, timeUnit, demandUnit);
 deltat = 15*unit("min");
 stopt  = min([Supply.Timeinfo.End, Demand.Timeinfo.End]);
 
+
 %% System parameters
 
 % transport from supply
@@ -34,27 +35,44 @@ aInjection = 0.0; % Dissipation coefficient
 EStorageMax     = 107.*unit("MWh"); % Maximum energy
 EStorageMin     = 0.0*unit("MWh"); % Minimum energy
 EStorageInitial = 0.0*unit("MWh"); % Initial energy
+
+%Tank parameters
 ro = 997; % density of tank liquid
 c = 4184; % heat capacity of tank liquid
-dT = 77; % temperature difference tank and outside
-x = 0.2; % shell thickness
-d = 0.3; % insulation thickness
-hw = 500; % convective heat transfer coeficient of water
-ks = 15; % conductive heat transfer coeficient of steel
-ki = 0.05; % conductive heat transfer coeficient of insulation
+dT = 77; % temperature difference tank and outside range: 67-77
+sig = 5.67e-8; % stephan boltzman constant 
+
+%Thermal coeficients and thicknesses
+hw = 500; % convective heat transfer coeficient of water range:50-3000
+ha = 200; % convective heat transfer coeficient of air range: 10-500
+ks = 15; % conductive heat transfer coeficient of steel 
+ki = 0.05; % conductive heat transfer coeficient of insulation range:0.02-0.05
 kg = 1; % conductive heat transfer coeficient of ground
+x = 0.2; % shell thickness range: 0.1-1
+d = 0.3; % insulation thickness range: 0-1
+eps = 0.9; % emissivity of insulating material 0.8-0.95
+Ta = 15; % Temperature of air
+Ts = 90; % surface temperature of tank
+hr = (sig*eps*(Ts+Ta)*(Ts^2+Ta^2)); % coeficient of the radiation loss
+
+%Geometry
 V = EStorageMax/(ro*c*dT); % volume of the tank 
 ri = ((3*V)/(4*pi))^(1/3); % inner radius of the tank
 r2=ri+x+d; % outer radius of the tank
 Ai = 4*pi*ri^2; % inner area of the tank
 A2 = 4*pi*r2^2; % outer area of the tank
+
+%Resistances
 Rb = (1/(hw*Ai)); % resistance of water to steel 
 Rs = (1/(4*pi*ks))*((1/ri)-(1/(ri+x))); % resistance of the steel layer
 Ri = (1/(4*pi*ki))*((1/(ri+x))-(1/(r2))); % resistance of the insulation layer
-Rc = (1/(4*pi*kg*r2)); % resistance of the ground
-Rr = 0; % resistance of the radiation loss
-Rt = Rb+Rs+Ri+Rc+Rr; % total resitance
+Rc = (1/(kg*A2)); % resistance of the ground
+Rsur = 0*(1/((hr+ha)*A2)); % Resistance of the surface when sphere is outside
+Rt = Rb+Rs+Ri+Rc+Rsur; % total resitance
+
+%Storage dissipation coeficcient
 bStorage = (1/(ro*V*c*Rt))/unit("s");  % Storage dissipation coefficient
+
 
 % extraction system
 aExtraction = 0.6; % Dissipation coefficient
