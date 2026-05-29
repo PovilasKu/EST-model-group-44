250266dc99eeebfc7a24ee0f0e8466a657f96866
Broekhoven, Jens van <j.g.a.m.v.broekhoven@student.tue.nl>
2026-05-29 13:11:10 +0200

    added units as comments, made two different parameters for outside and inside tank

--- preprocessing.m
+++ preprocessing.m
@@ -37,37 +37,38 @@ EStorageMin     = 0.0*unit("MWh"); % Minimum energy
 EStorageInitial = 0.0*unit("MWh"); % Initial energy
 
 %General parameters
-ro = 997; % density of tank liquid
-c = 4184; % heat capacity of tank liquid
-Tmax = 363.15; 
-Tground = 285.15;
-Twater = 285.15;
+ro = 997; % density of tank liquid (kg/m^3)
+c = 4184; % heat capacity of tank liquid (J/(kg*K)
+Tmax = 363.15; % maximum temperature of the liquid (K)
+Tground = 285.15; % ground temperature (K)
+Twater = 285.15; % water temperature (K)
 
 %Thermal coeficients and thicknesses
-ha = 50; % convective heat transfer coeficient of air range: 10-500
-ks = 15; % conductive heat transfer coeficient of steel 
-ki = 0.05; % conductive heat transfer coeficient of insulation range:0.02-0.05
-kg = 1; % conductive heat transfer coeficient of ground
-x = 0.2; % shell thickness range: 0.1-1
-d = 0.1; % insulation thickness range: 0.05-1
+ha = 50; % convective heat transfer coeficient of air (W/(m^2*K) range: 10-500
+ks = 15; % conductive heat transfer coeficient of steel (W/(m*K)
+ki = 0.05; % conductive heat transfer coeficient of insulation (W/(m*K) range:0.02-0.05
+kg = 1; % conductive heat transfer coeficient of ground (W/(m*K)
+x = 0.2; % shell thickness (m) range: 0.1-1
+d = 0.1; % insulation thickness (m) range: 0.05-1
 
 
 %Geometry
-V = ((EStorageMax*3.6e9)/(ro*c*(Tmax-Tground))); % volume of the tank 
-r1 = ((3*V)/(4*pi))^(1/3); % inner radius of the tank
-r2 = (r1+x); % radius of the tank + steel layer
-r3 = (r2+d); % radius of the tank + steel + insulation layer
-rinf = 5; % radius of the infinite ground
+V = ((EStorageMax*3.6e9)/(ro*c*(Tmax-Tground))); % volume of the tank (m^3) 
+r1 = ((3*V)/(4*pi))^(1/3); % inner radius of the tank (m)
+r2 = (r1+x); % radius of the tank + steel layer (m)
+r3 = (r2+d); % radius of the tank + steel + insulation layer (m)
+rinf = 5; % radius of the infinite ground (m)
 
 %Resistances
-Rs = ((1/(4*pi*ks))*((1/r1)-(1/(r2)))); % resistance of the steel layer
-Ri = ((1/(4*pi*ki))*((1/(r2))-(1/(r3)))); % resistance of the insulation layer
-Rc = ((1/(4*pi*kg))*((1/r3)-(1/rinf))); % resistance of the ground
-Ra = (0*(1/(4*pi*ha*r3))); % Resistance of the surface when sphere is outside
-Rt = (Rs+Ri+Rc+Ra); % total resitance
+Rs = ((1/(4*pi*ks))*((1/r1)-(1/(r2)))); % resistance of the steel layer (K/W)
+Ri = ((1/(4*pi*ki))*((1/(r2))-(1/(r3)))); % resistance of the insulation layer (K/W)
+Rc = ((1/(4*pi*kg))*((1/r3)-(1/rinf))); % resistance of the ground (K/W)
+Ra = ((1/(4*pi*ha*r3))); % Resistance of the surface when sphere is outside (K/W)
+Rtb = (Rs+Ri+Rc); % total resitance for burried tank (K/W)
+Rto = (Rs+Ri+Ra); % total resistance for outside tank (K/W)
 
 %Storage dissipation coeficcient
-bStorage = (1/(ro*V*c*Rt))/unit("s");  % Storage dissipation coefficient
+bStorage = (1/(ro*V*c*Rtb))/unit("s");  % Storage dissipation coefficient
 
 
 % extraction system
